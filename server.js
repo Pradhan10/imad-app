956a7ee87e637f20f2147afa371cd4e40c665889
Pradhan10 <pradhanrishi10@gmail.com>
2018-02-14 22:59:35 +0530

    changes to server js

--- server.js
+++ server.js
@@ -13,11 +13,13 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/fox.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'fox.png'));
 });
 
-
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
