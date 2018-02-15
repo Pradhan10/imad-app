86b417db5496bc55d8a8327e9a9ef67667814bbe
Pradhan Rishi Sharma <pradhanrishi10@gmail.com>
2018-02-15 11:22:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,2 @@
 console.log('Loaded!');
 
-var button = document.getElementById("clickme");
-var count = 0;
-button.onclick = function(){
-    // make request to counter endpoint 
-    // capture response and share result
-    // update html
-    count = count + 1;
-    var display = document.getElementById("display").innnerHTML=count.toString();
-};
