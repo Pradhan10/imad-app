da1d0c3e01ab9ad99002ecea6e1de7cd029d313c
Pradhan Rishi Sharma <pradhanrishi10@gmail.com>
2018-02-15 11:45:57 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,21 @@
 console.log('Loaded!');
-var button = document.getElementById('clickme');
+var button = document.getElementById("clickme");
 var count = 0;
 button.onclick = function(){
-    // make request to counter endpoint 
-    // capture response and share result
-    // update html
-    count = count + 1;
-    var display = document.getElementById('display').innerHTML=count.toString();
+    // Create request object
+    var request = new XMLHttpRequest()
+    // capture response and store it in variable
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE){
+            //take some action
+            if(request.status === 200){
+                var counter = request.responseText;
+                var display = document.getElementById("display").innnerHTML=count.toString();
+            }
+        } //if request is not done, do nothing
+    };
+    
+    // make request
+    request.open("GET","http://pradhanrishi10.imad.hasura-app.io/counter",true);
+    request.send(null);
 };
