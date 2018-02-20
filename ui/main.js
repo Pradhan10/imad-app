f1b2246cb6c316a9675fb9d3dd571a3612902a8c
Pradhan10 <pradhanrishi10@gmail.com>
2018-02-20 19:09:00 +0530

    Changes to main.js and index.html

--- ui/main.js
+++ ui/main.js
@@ -1,21 +1,15 @@
-console.log('Loaded!');
-var button = document.getElementById("clickme");
-var count = 0;
-button.onclick = function(){
-    // Create request object
-    var request = new XMLHttpRequest();
-    // capture response and store it in variable
-    request.onreadystatechange = function() {
-        if (request.readyState === XMLHttpRequest.DONE){
-            //take some action
-            if(request.status === 200){
-                var counter = request.responseText;
-                var display = document.getElementById("display").innnerHTML=count.toString();
-            }
-        } //if request is not done, do nothing
-    };
-    
-    // make request
-    request.open("GET","http://pradhanrishi10.imad.hasura-app.io/counter",true);
-    request.send(null);
+//counter code
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function() {
+
+	//Make a request to counter endpoint
+	
+	//Capture the response and store it in a variable 
+	
+	//Render the variable in correct span
+	counter = counter + 1;
+	var span = document.getElementById('count');
+	span.innerHTML = counter.toString();
+
 };
