1ef3cef7719fa3455249885893450a7e818109d4
Pradhan10 <pradhanrishi10@gmail.com>
2018-02-20 19:25:11 +0530

    main.js counter endpoint

--- ui/main.js
+++ ui/main.js
@@ -3,13 +3,23 @@ var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function() {
 
-	//Make a request to counter endpoint
-	
+	//Create a request
+	var request = new XMLHttpRequest();
 	//Capture the response and store it in a variable 
+	request.onreadystatechange = function() {
+		if (request.readyState === XMLHttpRequest.DONE){
+			//Take some action.
+			if (request.status === 200){
+				var counter = request.responseText;
+				var span = document.getElementById('count');
+				span.innerHTML = counter.toString();
+			}
+		}
+		// Not done yet.
 	
-	//Render the variable in correct span
-	counter = counter + 1;
-	var span = document.getElementById('count');
-	span.innerHTML = counter.toString();
-
+	};
+	
+	//Make a request
+	request.open('GET','http://pradhanrishi10.imad.hasura-app.io/counter',true);
+	request.send(null);
 };
