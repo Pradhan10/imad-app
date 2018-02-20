8a8f071af0fdda80fcb239eacce3d7fd4f491720
Pradhan10 <pradhanrishi10@gmail.com>
2018-02-21 01:36:04 +0530

    server.js changed

--- server.js
+++ server.js
@@ -34,6 +34,19 @@ app.get('/counter', function (req, res) {
   counter = counter + 1;    
   res.send(counter.toString());
 });
+
+var nammes = [];
+app.get('/submit-name/:name', function(req, res) {
+
+	//Get the name from request
+	var name = req.params.name;
+	
+	names.push(name);
+	// use JSON to convert list to string
+	res.send(JSON.stringify(names));
+	
+});
+
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
