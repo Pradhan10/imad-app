f0aeb24cf1bf53cb4b13b04d2b7a4d1ddb25d465
Pradhan Rishi Sharma <pradhanrishi10@gmail.com>
2018-03-12 17:43:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ var crypto = require('crypto');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 
-var config = {
-    user: 'coco98',
-    database: 'coco98',
-    host: 'db.imad.hasura-app.io',
-    port: '5432',
-    password: process.env.DB_PASSWORD
+var config ={
+	user: 'pradhanrishi10',
+	database: 'pradhanrishi10',
+	host: 'db.imad.hasura-app.io', 
+	port: '5432', 
+	password: process.env.DB_PASSWORD
 };
 
 var app = express();
